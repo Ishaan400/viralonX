36f9ce0ee568e819c4284d28758584e03b688006
Ishaan Kumar <ishname200@gmail.com>
2025-07-25 18:36:07 +0530

    Unify routing: use App Router (or Pages Router) exclusively

--- pages/404.js
+++
@@ -1,9 +0,0 @@
-// pages/404.js
-export default function Custom404() {
-  return (
-    <div style={{ textAlign: 'center', padding: '4rem' }}>
-      <h1>404 – Page Not Found</h1>
-      <p>Oops! We can’t find what you’re looking for.</p>
-    </div>
-  );
-}
