3c1aa714755da50128225b62cf96137de1c22514
Ishaan Kumar <ishname200@gmail.com>
2025-07-25 18:01:32 +0530

    Add custom 404 and 500 pages to override default Html imports

---
+++ pages/500.js
@@ -0,0 +1,9 @@
+// pages/500.js
+export default function Custom500() {
+  return (
+    <div style={{ textAlign: 'center', padding: '4rem' }}>
+      <h1>500 – Server-side Error</h1>
+      <p>Sorry, something went wrong on our end.</p>
+    </div>
+  );
+}
